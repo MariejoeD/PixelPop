5f64e5d8e43ace306475cd01252417f63df5caea
dominique-bautista <68311023+dominique-bautista@users.noreply.github.com>
2025-02-18 13:29:33 +0000

    webcam

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 function openFileManager() {
     alert("File Manager would open here.");
 }
+async function startWebcam() {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                document.getElementById('webcam').srcObject = stream;
+            } catch (err) {
+                console.error("Error accessing webcam: ", err);
+            }
+        }
+
+        startWebcam();
